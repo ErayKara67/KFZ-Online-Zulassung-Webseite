2bcf6dccadb71386e9d261b5e9369c64f6279e83
Gigas Chads <cahid1993@googlemail.com>
2026-08-30 02:46:06 +0200

    delete

--- src/app/umweltplakette/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ServicePage, serviceMetadata } from "@/components/service-page";
-
-export const metadata = serviceMetadata("umweltplakette");
-
-export default function Page() {
-  return <ServicePage slug="umweltplakette" />;
-}
--- src/app/wunschkennzeichen/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ServicePage, serviceMetadata } from "@/components/service-page";
-
-export const metadata = serviceMetadata("wunschkennzeichen");
-
-export default function Page() {
-  return <ServicePage slug="wunschkennzeichen" />;
-}
